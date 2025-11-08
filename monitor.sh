98df022e8dc96df08a8ab1bcf782279e32ccf1bb
PranavMish30 <pranavmish30@gmail.com>
2025-11-08 16:34:39 +0530

    updated monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -4,16 +4,16 @@ do
     echo "Starting monitoring cycle..."
     
     # 1. Packet Capture (e.g., for 60 seconds)
-    python ./Agent/pcap.py -i wlan0 -t 60 -o /Packets/output.pcap
+    python3 ./Agent/pcap.py -i wlan0 -t 60 -o /Packets/output.pcap
 
     # 2. Preprocessing & Cleaning
-    python ./Agent/pre-processing.py
+    python3 ./Agent/pre-processing.py
 
     # 3. Feature Generation
-    python ./Agent/feature-aggregation.py
+    python3 ./Agent/feature-aggregation.py
 
     # 4. Prediction & Alerting
-    python ./Agent/prediction.py
+    python3 ./Agent/prediction.py
     
     # Pause before the next cycle (e.g., 5 seconds)
     echo "Cycle complete. Waiting for 15 seconds..."
